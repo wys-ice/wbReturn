229404fd61c1df8601f83daa90109717591aa976
wangyongsheng <1090469606@qq.com>
2020-08-10 14:04:08 +0800

    one commit

---
+++ src/components/one.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>one commit</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scroll',
+  data () {
+    return {
+      textArr: [
+        '1 第一条公告',
+        '2 第二条公告第二条公告',
+        '3 第三条公告第三条公告第三条公告',
+        '4 one commit'
+      ],
+      number: 0,
+      timer: null
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/tipTranstion.vue
+++ src/components/tipTranstion.vue
@@ -22,7 +22,8 @@ export default {
       textArr: [
         '1 第一条公告',
         '2 第二条公告第二条公告',
-        '3 第三条公告第三条公告第三条公告'
+        '3 第三条公告第三条公告第三条公告',
+        '4 one commit'
       ],
       number: 0,
       timer: null
